750826093fbb7c1823e99ec9bcf0e70bdb539f68
RKOpTris <mukappa@Mus-MacBook-Pro.local>
2024-04-14 14:28:13 +0100

    adding separate Python solution 002

---
+++ 0002_python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+div2 = []
+fib_nums = [1, 1]
+fib_max = 0
+
+while fib_max < 4e6:
+    new_num = sum(fib_nums)
+    fib_max = new_num
+    if new_num % 2 ==0:
+        div2.append(new_num)
+    
+    fib_nums = [fib_nums[1], sum(fib_nums)] 
+    
+print(div2)
+print(sum(div2))
+
