48c792519e169cea0005de8b2d19a4902e42f234
RKOpTris <mukappa@Mus-MacBook-Pro.local>
2024-04-14 13:26:56 +0100

    adding Python solutions

---
+++ pe_solutions_python.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#25
+fib_last2 = [1, 1]
+fib_max = 0
+index = len(fib_last2)
+
+while len(str(fib_max)) < 1000:
+    new_max = sum(fib_last2)
+    fib_last2 = [fib_last2[1], new_max]
+    fib_max = new_max
+    index += 1
